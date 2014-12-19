2d08879b1107c2c175270be91706d5ec5e4ef4e8
adamhrz <adam.howitt@raizlabs.com>
2014-12-19 12:48:58 -0500

    Starter code for bonjour

--- Party Playlist/AppDelegate.m
+++ Party Playlist/AppDelegate.m
@@ -9,7 +9,33 @@
 #import "AppDelegate.h"
 #import "PPPlaylistModel.h"
 
-@interface AppDelegate ()
+// The Bonjour service type consists of an IANA service name (see RFC 6335)
+// prefixed by an underscore (as per RFC 2782).
+//
+// <http://www.ietf.org/rfc/rfc6335.txt>
+//
+// <http://www.ietf.org/rfc/rfc2782.txt>
+//
+// See Section 5.1 of RFC 6335 for the specifics requirements.
+//
+// To avoid conflicts, you must register your service type with IANA before
+// shipping.
+//
+// To help network administrators indentify your service, you should choose a
+// service name that's reasonably human readable.
+
+static NSString * kWiTapBonjourType = @"_partyplaylist1._tcp.";
+
+@interface AppDelegate () <NSNetServiceDelegate,
+NSStreamDelegate>
+
+@property (nonatomic, strong, readwrite) NSNetService *server;
+@property (nonatomic, assign, readwrite) BOOL                   isServerStarted;
+@property (nonatomic, copy,   readwrite) NSString *             registeredName;
+@property (nonatomic, strong, readwrite) NSInputStream *        inputStream;
+@property (nonatomic, strong, readwrite) NSOutputStream *       outputStream;
+@property (nonatomic, assign, readwrite) NSUInteger             streamOpenCount;
+
 
 @end
 
@@ -18,6 +44,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.server = [[NSNetService alloc] initWithDomain:@"local." type:kWiTapBonjourType name:[UIDevice currentDevice].name port:0];
+    self.server.includesPeerToPeer = YES;
+    [self.server setDelegate:self];
+    [self.server publishWithOptions:NSNetServiceListenForConnections];
+    self.isServerStarted = YES;
+
     return YES;
 }
 
@@ -26,15 +58,44 @@
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+#pragma unused(application)
+    
+    // If there's a game playing, shut it down.  Whether this is the right thing to do
+    // depends on your app.  In some cases it might be more sensible to leave the connection
+    // in place for a short while to see if the user comes back to the app.  This issue is
+    // discussed in more depth in Technote 2277 "Networking and Multitasking".
+    //
+    // <https://developer.apple.com/library/ios/#technotes/tn2277/_index.html>
+    
+    if (self.inputStream) {
+//        [self setupForNewGame];
+    }
+    
+    // Quiesce the server and service browser, if any.
+    
+    [self.server stop];
+    self.isServerStarted = NO;
+    self.registeredName = nil;
+//    if (self.picker != nil) {
+//        [self.picker stop];
+//    }
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+#pragma unused(application)
+    
+    // Quicken the server.  Once this is done it will quicken the picker, if there's one up.
+    
+    assert( ! self.isServerStarted );
+    [self.server publishWithOptions:NSNetServiceListenForConnections];
+    self.isServerStarted = YES;
+    if (self.registeredName != nil) {
+//        [self startPicker];
+    }
 }
-
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
