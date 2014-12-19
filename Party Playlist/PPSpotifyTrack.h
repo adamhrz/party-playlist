b8597a164af5d6a43412988b1ee6c223430bccfd
Derek Ostrander <derek@raizlabs.com>
2014-12-19 12:40:12 -0500

    Spotify setup

---
+++ Party Playlist/PPSpotifyTrack.h
@@ -0,0 +1,13 @@
+//
+//  PPSpotifyTrack.h
+//  Party Playlist
+//
+//  Created by Derek Ostrander on 12/19/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "PPTrack.h"
+
+@interface PPSpotifyTrack : PPTrack
+
+@end
